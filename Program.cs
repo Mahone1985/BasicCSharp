afc426cbf9c7272b7d6c629e93538d193d09441c
Tom Honeyfield <tomhoneyfield@hotmail.com>
2021-09-30 20:53:05 +0100

    C# v2

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace BasicCSharp
             int theVoid;
             double heatDeath;
             int theAnswer;
+            DateTime today = DateTime.Today;
+
 
             // What happens when you divide by zero? Not much
             Console.WriteLine("Which number can you divide by zero?");
@@ -17,6 +19,7 @@ namespace BasicCSharp
             Console.WriteLine("Another??");
             theVoid = Convert.ToInt32(Console.ReadLine());
             heatDeath = theAnswer / theVoid;
+            Console.WriteLine("Updated Today {0}", today);
             Console.WriteLine("You can't D/0 {0} by {1}, because the answer would be {2}", theAnswer, theVoid, heatDeath);
 
         }
