cbb09bb73a91e103309a29f074079f072e581f04
Tom Honeyfield <tomhoneyfield@hotmail.com>
2021-09-30 20:31:16 +0100

    C# v1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasicCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables
+            int theVoid; 
+            double heatDeath;
+            int theAnswer;
+
+            // What happens when you divide by zero? Not much
+            Console.WriteLine("Which number can you divide by zero?");
+            theAnswer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Another??");
+            theVoid = Convert.ToInt32(Console.ReadLine());
+            heatDeath = theAnswer / theVoid;
+            Console.WriteLine("You can't D/0 {0} by {1}, because the answer would be {2}", theAnswer, theVoid, heatDeath);
+
+        }
+    }
+}
